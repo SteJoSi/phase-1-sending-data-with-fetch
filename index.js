85d34e803d0e6a517b356892a5941d4f39d86034
Stephanie Sison <sjsison27@icloud.com>
2022-06-18 00:29:33 -0700

    commit tree f3cae0d09f452b9bb132a7da5d376fbc69da3086

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+    },
+    body: JSON.stringify({
+        name: `${userName}`,
+        email: `${userEmail}`,
+    })
+};
+return fetch("http://localhost:3000/users", configObj)
+    .then(res => res.json())  
+    .then(body => addNewContact(body.id))
+
+    .catch(alertMessage => {
+        alert(`Unauthorized Access`)
+        console.log(alertMessage)
+        document.body.append(alertMessage)
+    })
+
+}
+
+function addNewContact(newID) {
+    document.getElementsByTagName("body").innerHTML = newID;
+    document.body.append(newID)
+}
+
+// function errorMessage(errors) {
+//     document.getElementsByTagName("body").innerHTML = errors.message;
+//     document.body.append(errors)
+// }
